0bd23da03353caaceb2253a66a520c9ea3fd1f68
Clint J Edwards <clint.j.edwards@gmail.com>
2018-05-06 19:06:27 -0400

    Moving to github

--- app/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-#Disable warning: Import should be placed at the top of the module pylint: disable=C0413
-
-app = Flask(__name__)
-app.debug = False
-
-from app import views
